3f0d3c803a6c5e6baea79c9a564b99a0751be7f5
Christian Andres Arteaga <caarteagag@gmail.com>
2017-06-01 13:27:43 -0500

    First Commit

---
+++ DLLInjector/Config.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace DLLInjector
+{
+    public class Config
+    {
+        string Cfg = "InjectorCfg.ini";
+        public string DLLPath = "";
+        public string PrcName = "";
+        public bool Console = false;
+        public bool Close = false;
+
+        public void CheckConfig()
+        {
+            if (File.Exists(Cfg))
+            {
+                var parser = new FileIniDataParser();
+                IniData data = parser.ReadFile(Cfg);
+                DLLPath = data["Configuration"]["Dll"];
+                PrcName = data["Configuration"]["Process"];
+                Console = Convert.ToBoolean(data["Configuration"]["Console"]);
+                Close = Convert.ToBoolean(data["Configuration"]["Close"]);                
+            }
+            else
+            {
+                CreateConfig();
+            }            
+        }
+        public void SaveConfig()
+        {
+            var parser = new FileIniDataParser();
+            IniData data = new IniData();
+            data["Configuration"]["Dll"] = DLLPath;
+            data["Configuration"]["Process"] = PrcName;
+            data["Configuration"]["Console"] = Console.ToString();
+            data["Configuration"]["Close"] = Close.ToString();
+
+            parser.WriteFile(Cfg, data);
+        }
+        void CreateConfig()
+        {
+            File.Create(Cfg).Dispose();
+
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(Cfg);            
+            data.Sections.AddSection("Configuration");
+            data["Configuration"].AddKey("Dll", "");
+            data["Configuration"].AddKey("Process", "");
+            data["Configuration"].AddKey("Console", Console.ToString());
+            data["Configuration"].AddKey("Close", Close.ToString());
+
+            parser.WriteFile(Cfg, data);
+        }
+    }
+
+    
+}
---
+++ DLLInjector/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Security;
+
+
+
+namespace DLLInjector
+{
+   
+        /// <summary>
+        /// Lógica de interacción para MainWindow.xaml
+        /// </summary>
+        /// 
+
+        public partial class MainWindow : Window
+    {
+        Config config = new Config();
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        //DragMove Function
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        //DragDrop DLL
+        private void window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);                
+                if (System.IO.Path.GetExtension(files[0]) == ".dll")
+                {
+                    DllTextBox.Text = System.IO.Path.GetFileName(files[0]);
+                    config.DLLPath  =  files[0]; 
+                }                               
+            }
+        }
+
+        //Console Show/Hide
+        private void CheckConsole_Click(object sender, RoutedEventArgs e)
+        {
+            config.Console = CheckConsole.IsChecked.Value;
+            if (CheckConsole.IsChecked == true)
+            {
+                ConsoleManager.Show();                
+            }
+            else
+            {
+                ConsoleManager.Hide();
+                
+            }
+        }
+        //CheckBox Proc closed
+        private void CheckProcExit_Click(object sender, RoutedEventArgs e)
+        {
+            config.Close = CheckProcExit.IsChecked.Value;
+        }
+
+        //Enable InjectBT
+        private void ProcTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ProcTextBox.Text.Length> 3 && DllTextBox.Text.Length > 3)
+            {
+                InjectBT.IsEnabled = true;
+                BTShadow.Opacity = 1;
+            }
+            else
+            {
+                InjectBT.IsEnabled = false;
+                BTShadow.Opacity = 0;
+            }
+        }
+        private void DllTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ProcTextBox.Text.Length > 3 && DllTextBox.Text.Length > 3)
+            {
+                InjectBT.IsEnabled = true;
+                BTShadow.Opacity = 1;
+            }
+            else
+            {
+                InjectBT.IsEnabled = false;
+                BTShadow.Opacity = 0;
+            }
+        }
+
+        //Load config
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            config.CheckConfig();
+            ProcTextBox.Text = config.PrcName;
+            DllTextBox.Text = System.IO.Path.GetFileName(config.DLLPath);
+            CheckConsole.IsChecked = config.Console;
+            CheckProcExit.IsChecked = config.Close;
+        }
+
+        //Inject
+        private void InjectBT_Click(object sender, RoutedEventArgs e)
+        {
+            config.PrcName = ProcTextBox.Text;
+            config.SaveConfig();
+        }
+    }
+}
